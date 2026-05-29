0823e1a16471745106a588ae04bbc079c2f259d7
SebastianHuaringa <>
2026-05-29 10:51:30 -0500

    Creacion del archivo Examen_Parcial

---
+++ src/main/java/com/mycompany/examen_parcial_sebastian_huaringa/Examen_Parcial_Sebastian_Huaringa.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_parcial_sebastian_huaringa;
+
+/**
+ *
+ * @author juane
+ */
+public class Examen_Parcial_Sebastian_Huaringa {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
